d7905dcedb71840cc340f2b8cf960fcbbd1733f5
matemos <marcocerbella4@gmail.com>
2026-02-05 21:37:33 +0100

    dioc

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { registerSW } from "virtual:pwa-register";
 
+import "./index.css";
+
 registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")).render(
