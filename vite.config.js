d53091a13be2fa9d26ce7a5f81db7c3bfe645807
matemos <marcocerbella4@gmail.com>
2026-02-05 21:32:09 +0100

    ab

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["vite.svg"],
+      manifest: {
+        name: "myPetter",
+        short_name: "myPetter",
+        description: "La mia prima PWA con React",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
